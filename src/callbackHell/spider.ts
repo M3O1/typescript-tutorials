cea4d28c0d93a7d471b8d483e7a2d5430e16e74b
craftsangjae <craftsangjae@toss.im>
2021-09-06 00:18:57 +0900

    테스트 용이한 환경으로 코드 재구성

--- src/callbackHell/spider.ts
+++ src/callbackHell/spider.ts
@@ -3,34 +3,39 @@ import path from "path";
 import superagent from 'superagent';
 import {urlToFilename} from "./utils";
 
-export function spider(url: string, cb: (err: NodeJS.ErrnoException | null, filename?: string, downloaded?: boolean) => void) {
+type callbackFn = (err: NodeJS.ErrnoException | null, filename?: string, downloaded?: boolean) => void;
+
+export function spider(url: string, cb: callbackFn) {
     const filename = urlToFilename(url)
     fs.access(filename, err => {
-        if (err && err.code === 'ENOENT') {
-            console.log(`Downloading ${url} into ${filename}`)
-            superagent
-                .get(url)
-                .end((err, res) => {
-                    if (err) {
-                        cb(err)
-                    } else {
-                        fs.mkdir(path.dirname(filename), {recursive: true}, err => {
-                            if (err) {
-                                cb(err)
-                            } else {
-                                fs.writeFile(filename, res.text, err => {
-                                    if (err) {
-                                        cb(err)
-                                    } else {
-                                        cb(null, filename, true)
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
-        } else {
-            cb(null, filename, false)
+        if (!err || err.code !== 'ENOENT') {
+            return cb(null, filename, false)
         }
+        download(url, cb, filename);
     })
+}
+
+function saveFile(filename: string, cb: callbackFn, content: string) {
+    fs.mkdir(path.dirname(filename), {recursive: true}, err => {
+        if (err) {
+            return cb(err)
+        }
+        fs.writeFile(filename, content, err => {
+            if (err) {
+                return cb(err)
+            }
+            cb(null, filename, true)
+        })
+    })
+}
+
+function download(url: string, cb: callbackFn, filename: string) {
+    superagent
+        .get(url)
+        .end((err, res) => {
+            if (err) {
+                return cb(err)
+            }
+            saveFile(filename, cb, res.text);
+        })
 }
--- test/callbackHell/spider.test.ts
+++ test/callbackHell/spider.test.ts
@@ -1,9 +1,22 @@
 import {spider} from "../../src/callbackHell/spider";
+import fs from "fs";
+import path from 'path';
 
-describe("Spider을 활용한 다운로드 확인", () => {
-    it("Spider을 통한 블로그 글 다운로드 테스트", (done) => {
-        spider("https://joshtronic.com/2021/01/17/recursively-create-directories-with-nodejs", (err, filename, downloaded) => {
+describe("Spider을 통한 블로그 글 다운로드 테스트", () => {
+    const expectedPath = 'www.typescriptlang.org/download.html';
+
+    afterAll(() => {
+        if (fs.existsSync(expectedPath)) {
+            fs.unlinkSync(expectedPath)
+            fs.rmdirSync(path.dirname(expectedPath), {recursive: true});
+        }
+    })
+
+    it("www.typescriptlang.org/download 글을 다운로드", (done) => {
+        spider("https://www.typescriptlang.org/download", (err, filename, downloaded) => {
+            expect(fs.existsSync(expectedPath)).toBeTruthy()
             done()
         })
     })
+
 })
